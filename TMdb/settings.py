d02bf31bf3bc6fc3f05b0a46b0325bd4c1a1e1f0
TR-Tanmoy <trtanmoy1@gmail.com>
2020-11-29 03:29:40 +0600

    Add detailview templates

--- TMdb/settings.py
+++ TMdb/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'TMdb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
