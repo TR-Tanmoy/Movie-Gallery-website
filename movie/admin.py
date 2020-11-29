8cd78ce57cf14965897f2d0ded7edcb5268c704b
TR-Tanmoy <trtanmoy1@gmail.com>
2020-11-29 12:52:24 +0600

    Add Movie links and Model

--- movie/admin.py
+++ movie/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Movie
+from .models import Movie , Movie_Links
 
 
 
 admin.site.register(Movie)
+admin.site.register(Movie_Links)
