16d51ec0f0a16bd55d0bda4b437155b4c1e4d39c
TR-Tanmoy <trtanmoy1@gmail.com>
2020-12-08 02:00:43 +0600

    Adding movie category

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
-from .views import MovieList, MovieDetail
+from .views import MovieList, MovieDetail, MovieCategory
+
+
+app_name='movie'
 
 urlpatterns = [
     path('', MovieList.as_view(), name='movie_list'),
+    path('category/<str:category>', MovieCategory.as_view(), name='movie_category'),
     path('<int:pk>', MovieDetail.as_view(), name='movie_detail'),
 ]
