ad94c3d1f9b49965adc327874e58532d95dd05b2
TR-Tanmoy <trtanmoy1@gmail.com>
2020-12-10 01:31:09 +0600

    Adding home pages

--- movie/views.py
+++ movie/views.py
@@ -6,10 +6,23 @@ from django.views.generic.dates import YearArchiveView
 from .models import Movie, Movie_Links
 
 
+
+class HomeView(ListView):
+    model = Movie
+    template_name='movie/home.html'
+
+
+    def det_context_data(self , **kwargs):
+        context = super(HomeView , self).get_context_data(**kwargs)
+        context['top_rated']=Movie.objects.filter(status='TR')
+        context['most_watched']=Movie.objects.filter(status='MW')
+        context['recently_added']=Movie.objects.filter(status='RA')
+        print(context)
+        return context
+
 class MovieList(ListView):
     model = Movie
     paginate_by = 2
-
 
 class MovieDetail(DetailView):
     model = Movie
@@ -24,7 +37,6 @@ class MovieDetail(DetailView):
         context = super(MovieDetail, self).get_context_data(**kwargs)
         context['links'] = Movie_Links.objects.filter(movie=self.get_object())
         context['related_movies']=Movie.objects.filter(category=self.get_object().category)#.order_by['created'][0:6]
-        print(context)
         return context
 
 
